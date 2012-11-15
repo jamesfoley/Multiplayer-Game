23db2903f58c37f5cda378f5c70b9f3673ef4983
James Foley <jamesrichmondfoley@googlemail.com>
2012-11-15 23:33:18 +0000

    Animation change

--- assets/js/game.js
+++ assets/js/game.js
@@ -41,10 +41,10 @@ function Game(){
 		//	Create player animation
 		var player_anim = new jaws.Animation({sprite_sheet: "/assets/img/players/default.png", frame_size: [27,32], frame_duration: 100});
         player.anim_default = player_anim.slice(0, 1);
-        player.anim_up = player_anim.slice(1, 4);
-        player.anim_down = player_anim.slice(4, 8);
-        player.anim_left = player_anim.slice(8, 12);
-        player.anim_right = player_anim.slice(12, 16);
+        player.anim_up = player_anim.slice(1, 5);
+        player.anim_down = player_anim.slice(5, 9);
+        player.anim_left = player_anim.slice(9, 13);
+        player.anim_right = player_anim.slice(13, 17);
 
         //	Set the player to idle
         player.setImage(player.anim_default.next());
