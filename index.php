00ca8b953aab63e8430e7ec39640d462b3676493
James Foley <jamesrichmondfoley@googlemail.com>
2012-11-15 21:53:32 +0000

    Added boostrap framework

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-We should be working now...
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Metadata -->
+		<title>James' Game</title>
+
+		<!-- Bootstrap -->
+		<link href="/assets/css/bootstrap.css" rel="stylesheet" media="screen" />
+
+		<!-- Javascript - I'm putting this here because I can -->
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		<script src="/assets/js/bootstrap.min.js"></script>
+
+	</head>
+
+	<body>
+		<h1>Hello, world!</h1>
+	</body>
+	
+</html>
