a22289e533e42c91203258457a222a442d042d6b
James Foley <jamesrichmondfoley@googlemail.com>
2012-11-16 10:45:40 +0000

    Set loadingscreen font

--- assets/js/jawsjs.js
+++ assets/js/jawsjs.js
@@ -183,9 +183,9 @@ jaws.start = function(game_state, options,game_state_setup_options) {
       jaws.context.fillRect(0, 0, jaws.width, jaws.height);
       jaws.context.textAlign  = "center"
       jaws.context.fillStyle  = "white"
-      jaws.context.font       = "15px Arial";
+      jaws.context.font       = "15px Calibri";
       jaws.context.fillText("Loading", jaws.width/2, jaws.height/2-30);
-      jaws.context.font       = "bold 30px terminal";
+      jaws.context.font       = "bold 30px Calibri";
       jaws.context.fillText(percent_done + "%", jaws.width/2, jaws.height/2);
       jaws.context.restore()
     }
